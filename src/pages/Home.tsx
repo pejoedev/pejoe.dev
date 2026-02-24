420c64ecff5d47309455a99732f2d1ecb0da5a03
pejoedev <jondevpet@gmail.com>
2026-02-24 20:49:07 +0100

    adding the cat

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import './Home.css'
 import ContentBlock from '../components/ContentBlock/ContentBlock'
 import Sidebar from '../components/Sidebar/Sidebar'
+import SpinningPFP from '../components/SpinningPFP/SpinningPFP'
 
 const Home: React.FC = () => {
     return (
         <div className="home">
             <section className="hero">
                 <ContentBlock>
-                    <h1>Welcome to Pejoe.dev</h1>
+                    <h1>🍁Welcome to Pejoe.dev</h1>
                     <p>
                         Welcome to my small website. This website is inspired by all these neocities sites.
                         This website is my blog, and my little digital garden.
@@ -18,7 +19,10 @@ const Home: React.FC = () => {
             <section className="sidebar">
                 <Sidebar></Sidebar>
                 <ContentBlock>
-
+                    <div style={{ margin: '20px auto', width: '140px' }}>
+                        <SpinningPFP src={'1mb_poes.png'} ></SpinningPFP>
+                        <p style={{ textAlign: 'center' }}>Click me!</p>
+                    </div>
                 </ContentBlock>
             </section>
 
